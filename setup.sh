af96f306abbed6b0731812eeecc9e851f2048cb6
atreyaved <70472507+atreyaved@users.noreply.github.com>
2021-08-23 18:19:19 +0530

    Added support for ZSH

--- setup.sh
+++ setup.sh
@@ -13,10 +13,40 @@ sleep 1
 mkdir ~/.local/share/NacreousDawn596/
 cd ..
 mv speechy/ ~/.local/share/NacreousDawn596/
-echo '' >> ~/.bashrc
-echo 'source ~/.local/share/NacreousDawn596/speechy/.speechy.sh' >> ~/.bashrc
-echo '' >> ~/.bashrc
-rm ~/.local/share/NacreousDawn596/speechy/setup.sh
-clear
-echo "done!"
-echo "now you can launch it by writing speechy after reopening the terminal ,enjoy!"
+
+echo Choose Your shell (Type the number)
+echo 1. Bash
+echo 2. Zsh
+
+read Choice
+
+Bash_def() {
+  echo '' >> ~/.bashrc
+  echo 'source ~/.local/share/NacreousDawn596/speechy/.speechy.sh' >> ~/.bashrc
+  echo '' >> ~/.bashrc
+  rm ~/.local/share/NacreousDawn596/speechy/setup.sh
+  clear
+  echo "done!"
+  echo "now you can launch it by writing speechy after reopening the terminal ,enjoy!"
+}
+
+Zsh_def ()
+  {
+    echo '' >> ~/.zshrc
+  echo 'source ~/.local/share/NacreousDawn596/speechy/.speechy.sh' >> ~/.zshrc
+  echo '' >> ~/.zshrc
+  rm ~/.local/share/NacreousDawn596/speechy/setup.sh
+  clear
+  echo "done!"
+  echo "now you can launch it by writing speechy after reopening the terminal ,enjoy!"}
+
+if [ $choice == 1 ]
+then
+  Bash_def()
+elif [ $choice == 2 ]
+then
+  Zsh_def()
+else
+  echo Shell not supported
+fi
+ 
